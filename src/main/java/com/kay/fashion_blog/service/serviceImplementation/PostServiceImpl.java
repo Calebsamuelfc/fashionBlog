c9e732619ac1521b82320df7e7ea0ec816a36c2e
calebsamuelfc <samcalluis@gmail.com>
2022-08-02 15:46:14 +0100

    updating workflows

--- src/main/java/com/kay/fashion_blog/service/serviceImplementation/PostServiceImpl.java
+++ src/main/java/com/kay/fashion_blog/service/serviceImplementation/PostServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -136,4 +137,5 @@ public class PostServiceImpl implements PostService {
         }
         return str;
     }
+
 }
